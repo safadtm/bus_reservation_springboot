1304a15fbf890e84a3a81e80ba1ece39ed536a8f
safadtm <safadtm2004@gmail.com>
2023-07-16 14:06:17 +0530

    Exception Handlers

---
+++ src/main/java/com/example/Bus/Reservation/models/ErrorDetails.java
@@ -0,0 +1,18 @@
+package com.example.Bus.Reservation.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+    private int errorCode;
+    private  String errorMessage;
+    private String devErrorMessage;
+    private long timeStamp;
+
+}
