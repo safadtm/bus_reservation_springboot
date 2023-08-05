d741f9efcda24270f134ca7233ceaa8cb5936e6f
safadtm <safadtm2004@gmail.com>
2023-08-05 09:28:22 +0530

    Spring security: modified security filter chain

--- src/main/java/com/example/Bus/Reservation/security/SecurityConfig.java
+++ src/main/java/com/example/Bus/Reservation/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.Bus.Reservation.security;
 
+import com.example.Bus.Reservation.services.CustomUserDetailsService;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,20 +10,44 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private JWTEntryPoint jwtEntryPoint;
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
+
    @Bean
    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
        http.authorizeHttpRequests((authReq)->authReq
                        .requestMatchers(HttpMethod.GET)
                        .permitAll()
-                       .anyRequest()
-                       .authenticated())
-               .httpBasic(Customizer.withDefaults());
+                       .requestMatchers("/api/auth/**")
+                       .permitAll()
+                       .requestMatchers(HttpMethod.POST,
+                               "/api/bus/add",
+                               "api/schedule/add",
+                               "api/route/add")
+                       .authenticated()
+                       .requestMatchers(HttpMethod.POST,
+                               "/api/reservation/add")
+                       .permitAll()
+
+                      )
+                      .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtEntryPoint))
+                      .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                      .httpBasic(Customizer.withDefaults());
+       http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
        return http.build();
    }
 
